8a3daaec5418c61d2c272df99d080425b774b404
Rossi21 <rossi21@gmail.com>
2023-05-07 07:33:14 -0700

    Second Commit

--- algos.js
+++ algos.js
@@ -3,7 +3,21 @@ function pushFront (arr, num) {
         arr[i] = arr[i-1];  
     }
     arr[0] = num;
+
     console.log(arr);
 }
 
-pushFront([5,7,2,3], 8);
+//pushFront([5,7,2,3], 8);
+//pushFront([99], 7);
+
+function popFront (arr) {
+    num = arr[0];
+    for(i =0;i < arr.length-1; i++){
+        arr[i] = arr[i+1];
+        arr.length = arr.length-1;
+    }
+    console.log(arr);
+    console.log(num);
+}
+
+popFront([0,5,10,15])
