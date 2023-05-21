06eab394c67a456cf715d290756a20569b6687a9
Rossi21 <rossi21@gmail.com>
2023-05-20 20:06:26 -0700

    display function

--- list.js
+++ list.js
@@ -11,17 +11,8 @@ class SLL {
         this.head = null;
     }
     addFront(val) {
-        // Creating a new node object with the value provided
         let new_node = new Node(val);
-        // Checking to see if the current list does not have a head node (if the list is empty)
-        // If the list is empty, place the new node as the head 
-        if(!this.head) {
-            this.head = new_node;
-            return this;
-        }
-        // If the list is not empty, assign the head to be the next node to the new node (Blue Arrow in picture above)
         new_node.next = this.head;
-        // Then finally assign the new_node to be the new head of the list (Red Arrow in picture above)
         this.head = new_node;
         return this;
     }
@@ -36,14 +27,87 @@ class SLL {
     front(){
         console.log(this.head.data);
     }
+    findSum(){
+        if (this.head === null){
+            return 0;
+        }
+        let runner = this.head;
+        let sum = 0;
+        while (runner !== null){
+            sum+=runner.data;
+            runner=runner.next;
+        }
+        return sum;
+    }
+    push(data) {
+        if (this.head === null){
+            this.head = new Node(data);
+            return;
+        }
+        let pointer = this.head;
+        while( pointer.next !== null){
+            pointer = pointer.next;
+        }
+        pointer.next = new Node(data);
+    }
+    contains(value){
+        if (this.head === null){
+            return null;
+        }
+        let runner = this.head;
+        while (runner !== null){
+            runner=runner.next;
+            if(runner.data === value){
+                return true;
+            }
+            return false;
+        }
+    }
+    length(){
+        if (this.head === null){
+            return null;
+        }
+        let runner = this.head;
+        let sum = 0;
+        while (runner !== null){
+            runner=runner.next;
+            sum++;
+        }
+        return sum;
+    }
+    display(){
+        if (this.head === null){
+            return null;
+        }
+        let runner = this.head; 
+        var string = "";
+        while (runner){
+            string += runner.data + ", ";
+            runner = runner.next;
+            } 
+            console.log(string);          
+        }
+    reverse(){
+        if (this.head === null){
+            return null;
+        }
+        let pointer = this.head;
+        let previous = null;
+        while(pointer !== null){
+            const temp = pointer.next;
+            pointer.next = previous;
+            previous = pointer;
+            pointer = temp;
+        }
+        this.head = previous;
+    }
 }
 
 var SLL1 = new SLL();
-SLL1.addFront(18).addFront(12);
-
+SLL1.addFront(10);
+SLL1.addFront(13);
+SLL1.push(12);
+SLL1.addFront(119);
 console.log(SLL1);
-
-SLL1.removeFront();
-
-console.log(SLL1);
+SLL1.display();
 
