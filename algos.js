4e8f5b578aef8c2af2d79b88d579d3c480595092
Rossi21 <rossi21@gmail.com>
2023-05-31 07:29:59 -0700

    leet code two sums

--- algos.js
+++ algos.js
@@ -209,7 +209,7 @@ function minToFront1(arr){
         arr[0] = min;
         console.log(arr);
 }
-minToFront([4,2,1,3,5])
+//minToFront([4,2,1,3,5])
 function minToFront(arr) {
     var min = arr[0];
     var idx = 0;
@@ -226,3 +226,18 @@ function minToFront(arr) {
     }
     return arr;
 }
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+var twoSum = function(nums, target) {
+    for(i=0; i<nums.length;i++){
+        for(j=0; j<nums.length;j++){
+            if(nums[i]+nums[j]==target)
+                return [i,j];
+        }  
+    }   
+};
+
+console.log(twoSum([3,2,4],6))
