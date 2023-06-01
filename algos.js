eef591c735680a656b6f030fff1e262644c84afb
Rossi21 <rossi21@gmail.com>
2023-06-01 07:03:31 -0700

    twoSum 2

--- algos.js
+++ algos.js
@@ -234,7 +234,7 @@ function minToFront(arr) {
 var twoSum = function(nums, target) {
     for(i=0; i<nums.length;i++){
         for(j=0; j<nums.length;j++){
-            if(nums[i]+nums[j]==target)
+            if(i!=j && nums[i]+nums[j]==target)
                 return [i,j];
         }  
     }   
