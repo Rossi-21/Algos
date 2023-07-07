b9780e520a8ddf4a98f4bcd677bd05f7e014f60b
Rossi21 <rossi21@gmail.com>
2023-07-07 08:39:17 -0700

    letter grade

--- algosNew.js
+++ algosNew.js
@@ -578,4 +578,31 @@ function finalIQ(){
     return IQ
 }
 
-console.log(finalIQ())
+//console.log(finalIQ())
+
+//Mr. Cerise teaches high school math. Write a function that assigns and prints a letter grade, 
+//given an integer representing a score from 0 to 100? 
+//Those getting 90+ get an ‘A’, 80-89 earn ‘B’, 70-79 is a ‘C’, 60-69 should get a ‘D’, 
+//and lower than 60 receive ‘F’. 
+//For example, given 88, you should log "Score: 88. Grade: B". 
+//Given the score 61, log the string "Score: 61. Grade: D".//
+
+function letterGrade(score){
+    if (score < 60){
+        console.log("Score: " + score + ". Grade: F")
+    }
+    else if( score > 59 && score < 70){
+        console.log("Score: " + score + ". Grade: D")
+    }
+    else if( score > 69 && score < 80){
+        console.log("Score: " + score + ". Grade: C")
+    }
+    else if( score > 79 && score < 90){
+        console.log("Score: " + score + ". Grade: B")
+    }
+    else if( score > 89 && score <= 100){
+        console.log("Score: " + score + ". Grade: A")
+    }
+}
+
+letterGrade(74)
